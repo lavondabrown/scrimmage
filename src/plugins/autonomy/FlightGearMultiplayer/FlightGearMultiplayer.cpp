29f7095589d75048a51e0e507b628688f315e4bf
Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
2018-03-23 13:07:05 -0400

    starting fgms plugin

---
+++ src/plugins/autonomy/FlightGearMultiplayer/FlightGearMultiplayer.cpp
@@ -0,0 +1,116 @@
+/*!
+ * @file
+ *
+ * @section LICENSE
+ *
+ * Copyright (C) 2017 by the Georgia Tech Research Institute (GTRI)
+ *
+ * This file is part of SCRIMMAGE.
+ *
+ *   SCRIMMAGE is free software: you can redistribute it and/or modify it under
+ *   the terms of the GNU Lesser General Public License as published by the
+ *   Free Software Foundation, either version 3 of the License, or (at your
+ *   option) any later version.
+ *
+ *   SCRIMMAGE is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ *   License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with SCRIMMAGE.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
+ * @author Eric Squires <eric.squires@gtri.gatech.edu>
+ * @date 31 July 2017
+ * @version 0.1.0
+ * @brief Brief file description.
+ * @section DESCRIPTION
+ * A Long description goes here.
+ *
+ */
+
+#include <scrimmage/plugins/autonomy/FlightGearMultiplayer/FlightGearMultiplayer.h>
+
+#include <scrimmage/plugin_manager/RegisterPlugin.h>
+#include <scrimmage/entity/Entity.h>
+#include <scrimmage/math/State.h>
+#include <scrimmage/parse/ParseUtils.h>
+
+#include <iostream>
+#include <limits>
+
+#include <GeographicLib/LocalCartesian.hpp>
+
+using std::cout;
+using std::endl;
+
+namespace sc = scrimmage;
+
+REGISTER_PLUGIN(scrimmage::Autonomy,
+                scrimmage::autonomy::FlightGearMultiplayer,
+                FlightGearMultiplayer_plugin)
+
+namespace scrimmage {
+namespace autonomy {
+
+FlightGearMultiplayer::FlightGearMultiplayer() {
+}
+
+void FlightGearMultiplayer::init(std::map<std::string, std::string> &params) {
+    // double server_ip = sc::get<std::string>("server_ip", params, "127.0.0.1");
+    // int server_port = sc::get<int>("server_port", params, 5000);
+
+    // TODO: Open a socket to the multiplayer server
+}
+
+bool FlightGearMultiplayer::step_autonomy(double t, double dt) {
+
+    // User Documentation about using FlightGear Multiplayer:
+    // http://wiki.flightgear.org/Howto:Multiplayer
+
+    // Developer documentation for the UDP packet that is sent from a Flight
+    // gear client to the multiplayer server:
+    // http://wiki.flightgear.org/Multiplayer_protocol
+
+
+    // Get lat, lon, alt from x, y, z position
+    double lat, lon, alt;
+    parent_->projection()->Reverse(state_->pos()(0), state_->pos()(1),
+                                   state_->pos()(2),
+                                   lat, lon, alt);
+
+    //////////////////////////////////////////////////////
+    // TODO: Create XDR packet from state_ variable.  We can leverage the
+    // socket code and XDR packet definitions in the actual flight gear
+    // multiplayer server code. So, in a directory (like ~/repos/3rd-party),
+    // clone the flight gear multiplayer server....
+    //
+    // git clone https://github.com/FlightGear/fgms.git
+    // build it... mkdir build && cd build && make
+    // We need to be able to use the files at:
+    // fgms/src/flightgear/MultiPlayer/
+
+    // Look at how the XDR_encode<T>() function is used in
+    // ./src/server/fg_server.cxx to create a message and send it with the
+    // m_DataSocket->sendto class. We need to fill in the appropriate data
+    // elements to create the flight gear position message (with information
+    // from our state_ variable) and send it to the multiplayer server using
+    // the m_DataSocket->sendto method. m_DataSocket is a netSocket class in
+    // the multiplayer server code.
+
+    // In order to use the multiplayer server code, this plugin needs to be
+    // able to "find" the fgms code. This can be handcoded at first, but
+    // eventually, I want this plugin to not build if the user didn't specify a
+    // CMake varaible that points to the fgms "build tree". For example, if the
+    // user wants to build this plugin, they should type:
+    // cmake .. -DFGMS_ROOT=~/repos/3rd-party/fgms
+    //
+    // If the user doesn't specify FGMS_ROOT variable, then this plugin
+    // shouldn't build.
+
+
+    return true;
+}
+} // namespace autonomy
+} // namespace scrimmage
